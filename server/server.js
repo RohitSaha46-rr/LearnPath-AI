41c651f87a7585fef419bd3e887b9192ea595d18
Rohit Saha <rhitsaha46@gmail.com>
2026-05-12 23:28:19 +0530

    production changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,11 @@ import { connectDB } from "./config/db.js"
 dotenv.config()
 connectDB();
 const app=express()
-app.use(cors())
+const allowedOrigins = [
+  'http://localhost:5173',
+  process.env.CLIENT_URL,
+].filter(Boolean);
+app.use(cors({ origin: allowedOrigins, credentials: true }))
 app.use(express.json())
 app.use("/api/auth",authRoutes)
 app.use("/api/roadmaps",roadmapRoutes)
